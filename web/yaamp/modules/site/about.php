cd6083aa3c42802730c5fdc989ab348bb30a3ae1
IdMineThat <100437916+IdMineThat@users.noreply.github.com>
2023-02-04 19:45:48 -0600

    Update about.php

--- web/yaamp/modules/site/about.php
+++ web/yaamp/modules/site/about.php
@@ -4,12 +4,12 @@
 
 <div style='padding-left: 25px; width: 640px;'>
 
-<p>cDirty Harry for all your YiiMP Server needs. Our team has been working in the crypto world for the last 2 years.
+<p>IdMineThat for all your YiiMP Server needs. Our team has been working in the crypto world for the last 2 years.
 We are pleased to offer everyone this free script to help you in setting up your own pools.</p>
 
-<p>Dirty Harry</p>
+<p>IdMineThat</p>
 
-<p>01/11/2020</p>
+<p>02/04/2023</p>
 
 </div>
 
